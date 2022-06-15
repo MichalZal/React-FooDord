6ddfd13da0aa856db8dbc79585f44e1e3e346c34
Michał Zalewski <78042518+MichalZal@users.noreply.github.com>
2022-06-15 22:38:14 +0200

    Update App.jsx

--- src/src/App.jsx
+++ src/src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
+
 const App = () => {
 	const [cartIsVisible, setCartIsVisible] = useState(false);
 
